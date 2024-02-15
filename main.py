5e3e7254046667f6c3ef8638991f001b6544ae0a
Ravi kiran Machha <92377897+Ravi14143@users.noreply.github.com>
2024-02-15 19:31:06 +0530

    main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def store_user_data(qr_code_id, password, email, name, gender, age, cursor):
 # Function to scan QR code using CV2
 def scan_qr_code():
     qr_code_id = None
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture('/dev/video0')
     while True:
         ret, frame = cap.read()
         if not ret:
